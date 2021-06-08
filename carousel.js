7e9411c4bdfd8fc2bb1651df7fe727f927c049a5
Citlallic <citlallicgutierrez@outlook.com>
2021-06-08 00:27:10 -0700

    seperate sections, and change background

--- carousel.js
+++ carousel.js
@@ -10,7 +10,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  setTimeout(showSlides, 9000); // Change image every 2 seconds
 }
 
 function plusSlides(n) {
