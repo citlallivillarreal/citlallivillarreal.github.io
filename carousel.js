54fe9c53d991daa98ee85687b4df8fc275f47989
Citlallic <citlallicgutierrez@outlook.com>
2021-06-07 22:41:09 -0700

    Add js file for slideshow and edit skills section and edit profile photo to be circular

---
+++ carousel.js
@@ -0,0 +1,23 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("images");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
